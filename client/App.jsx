4100cfe0c4546b006c8687118424f71c285d8e7e
Timothy Mai <timothy.mai13@gmail.com>
2019-09-21 11:53:40 -0700

    created App.jsx and index.js inside client folder, updated index.html

---
+++ client/App.jsx
@@ -0,0 +1,50 @@
+/**
+ * ************************************
+ *
+ * @module  App.jsx
+ * @author Timothy Mai
+ * @date 9/21/19
+ * @description the main body of the app
+ *
+ * ************************************
+ */
+
+import React, { Component } from 'react';
+// IMPORT CHILD COMPONENTS HERE
+
+class App extends Component {
+  constructor() {
+    super();
+
+    // state components
+    this.state = {
+      // ADD STATE COMPONENTS HERE
+    }
+
+    // binding functions to our object here
+  }
+
+  componentDidMount() {
+    // check if we need this
+  }
+
+  componentWillUnmount() {
+    // check if we need this
+  }
+
+  // add class methods here
+
+  render() {
+    // do stuff then return the app
+
+    return (
+      <div>
+        {/* add things inside this div */}
+        SUPER COOL APP
+      </div>
+    );
+  }
+
+}
+
+export default App;
---
+++ client/index.js
@@ -0,0 +1,19 @@
+/**
+ * ************************************
+ *
+ * @module  index.js
+ * @author Timothy Mai
+ * @date 9/21/19
+ * @description entry point for app. Hangs React app off of #app in index.html
+ *
+ * ************************************
+ */
+
+import React from 'react';
+import { render } from 'react-dom';
+import App from './App.jsx'; // this hasn't been created yet
+
+render(
+  <App />,
+  document.getElementById('app') // app is hung off the 'app' id in index.html
+);
